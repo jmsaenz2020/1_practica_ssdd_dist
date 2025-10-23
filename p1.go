ef4abb36f002af17d1eddc7d00e53dd8c42136ad
jsmnlsnz <jsmnlsnz@optiplex9020.local>
2025-10-23 12:37:31 +0200

    Metodos Menu Clase Taller

--- p1.go
+++ p1.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-
+  "fmt"
 )
 
 const MAX_PLAZAS = 2 // plazas por mecanico
@@ -15,6 +15,80 @@ type Taller struct{
   Clientes []Cliente
   Mecanicos []Mecanico
 }
+
+func (t Taller)MenuPrincipal(){
+  menu := []string{
+    "Opciones del taller",
+    "Lista de Incidencias",
+    "Lista de Incidencias de Mecánico",
+    "Lista de vehículos"}
+
+  opt, status := menuFunc(menu)
+
+  if (status == 0){
+    fmt.Println(menu[opt])
+  }
+}
+
+func (t Taller)MenuClientes(){
+  if (len(t.Clientes) > 0){
+    return
+  } else {
+    warningMsg("No hay clientes en el taller")
+  }
+}
+
+func (t Taller)ObtenerVehiculos() ([]Vehiculo){
+  var vehiculos []Vehiculo
+
+  for _, c := range t.Clientes{
+    for _, v := range c.Vehiculos{
+      vehiculos = append(vehiculos, v)
+    }
+  }
+
+  return vehiculos
+}
+
+func (t Taller)MenuVehiculos(){
+  vehiculos := t.ObtenerVehiculos()  
+
+  if (len(vehiculos) > 0){
+    return
+  } else {
+    warningMsg("No hay vehículos en el taller")
+  }
+}
+
+func (t Taller)MenuIncidencias(){
+  vehiculos := t.ObtenerVehiculos()
+  var incidencias []Incidencia
+
+  
+  if (len(vehiculos) > 0){
+    for _, v := range vehiculos{
+      for _, inc := range v.Incidencias{
+        incidencias = append(incidencias, inc)
+      }
+    }
+    if (len(incidencias) > 0){
+      return
+    } else {
+      warningMsg("No hay incidencias en el taller")
+    }
+  } else {
+    warningMsg("No hay vehiculos en el taller")
+  }
+}
+
+func (t Taller)MenuMecanicos(){
+  if(len(t.Mecanicos) > 0){
+    return
+  } else {
+    warningMsg("No hay mecánicos en el taller")
+  }
+}
+
 
 type Cliente struct{
   Id int
@@ -51,6 +125,63 @@ type Mecanico struct{
 }
 
 
-func main(){
+func warningMsg(msg string){
+  fmt.Printf("%s%s%s\n", YELLOW, msg, END)
+}
 
+func menuFunc(menu []string) (int, int){
+  var opt int
+
+  fmt.Printf("%s%s%s\n", BOLD, menu[0], END) // Menu title
+
+  for i:= 1; i < len(menu); i++{
+    fmt.Printf("%d.- %s\n", i, menu[i])
+  }
+
+  fmt.Print("> ")
+  fmt.Scanf("%d", &opt)
+
+  if (opt > 0 && opt < len(menu)){
+    return opt, 0
+  }
+  return 0, 1
+}
+
+func main(){
+  mainMenu := []string{
+    "Menu principal",
+    "Taller",
+    "Clientes",
+    "Vehiculos",
+    "Incidencias",
+    "Mecánicos",
+    "Salir"}
+
+  var t Taller
+  var exit bool = false
+
+  for{
+    opt, status := menuFunc(mainMenu)
+    if status == 0 { // ok
+      switch opt{
+        case 1:
+          t.MenuPrincipal()
+        case 2:
+          t.MenuClientes()
+        case 3:
+          t.MenuVehiculos()
+        case 4:
+          t.MenuIncidencias()
+        case 5:
+          t.MenuMecanicos()
+        case 6:
+          exit = true
+        default:
+          break
+      }
+    }
+    if (exit){
+      break
+    }
+  }
 }
